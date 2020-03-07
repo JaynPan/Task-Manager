b1874b8798f65a3871b10fb6beb17581ccf36034
Jiehen Pan <lb0104672@gmail.com>
2020-03-07 23:59:54 +0800

    Update gitignore and import eslint

--- mongodb.js
+++ mongodb.js
@@ -1,18 +1,20 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const mongodb = require('mongodb');
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager-api'
+const { MongoClient } = mongodb;
 
-MongoClient.connect(connectionURL,{ useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-  if(error) {
-    return console.log('unable to connect to database')
-  }
-  
-  const db = client.db(databaseName)
-  
-  db.collection('users').insertOne({
-    name: 'Andrew',
-    age: 27
-  })
-})
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager-api';
+
+MongoClient.connect(connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+      return console.log('unable to connect to database');
+    }
+
+    const db = client.db(databaseName);
+
+    return db.collection('users').insertOne({
+      name: 'Andrew',
+      age: 27,
+    });
+  });
