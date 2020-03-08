3c6e2f9f3691eec8b97feebda5e5585de7899175
Jiehen Pan <lb0104672@gmail.com>
2020-03-08 15:25:33 +0800

    Structure the first express mongoose app and using nodemon

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,54 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 6,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('Password cannot contain "password"');
-      }
-    },
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid');
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number');
-      }
-    },
-  },
-});
-
-const me = new User({
-  name: '   Jay    ',
-  password: 'dadsf',
-  email: 'hay-pan@gmail.com',
-});
-
-me.save().then((res) => console.log(res)).catch((err) => console.log(err));
