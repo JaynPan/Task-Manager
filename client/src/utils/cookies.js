8b10b1f858e71be885fff7d761efc4bdf50db2f0
Jiehen Pan <lb0104672@gmail.com>
2020-03-13 00:30:17 +0800

    Prettify client site files

--- client/src/utils/cookies.js
+++ client/src/utils/cookies.js
@@ -1,5 +1,13 @@
 export function getCookie(name) {
-  var value = "; " + document.cookie;
-  var parts = value.split("; " + name + "=");
-  if (parts.length === 2) return parts.pop().split(";").shift();
+  let value = "; " + document.cookie;
+  let parts = value.split("; " + name + "=");
+
+  if (parts.length === 2) {
+    return parts
+      .pop()
+      .split(";")
+      .shift();
+  }
+
+  return undefined;
 }
