9403de3ebf139c51fb5ee6e89f818459f8634a80
Jiehen Pan <lb0104672@gmail.com>
2020-03-21 19:50:17 +0800

    Deploy to heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
 });
+
+mongoose.set('useCreateIndex', true);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 const app = express();
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
+const publicDirectoryPath = path.join(__dirname, '../client/build');
 
+app.use(express.static(publicDirectoryPath));
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
