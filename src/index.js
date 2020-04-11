6266e66dc89495a6bdb250808f149b4837ba0507
Jiehen Pan <lb0104772@gmail.com>
2020-04-11 16:47:20 +0800

    Deploy to heroku

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.listen(port, () => {
   console.log('Server is up on port', port);
 });
